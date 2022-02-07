3c289981d9641fd8b88c61f11412c82f10ede714
minshxxx <minshxxx@gmail.com>
2022-02-07 13:23:49 +0900

    7 5 React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>hello</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
