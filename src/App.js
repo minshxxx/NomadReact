5346e6dca96582aa81b2081d090e352427bbd8a1
minshxxx <minshxxx@gmail.com>
2022-02-07 17:21:11 +0900

    7 7 Publishing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>hello</h1>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path={`/hello`} element={<h1>hello</h1>} />
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/movie/:id`} element={<Detail />} />
       </Routes>
     </Router>
   );
